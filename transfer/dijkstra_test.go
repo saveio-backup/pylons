abc3722b52ca18d667bc1bf30c7d6b8ee2368cbd
Guo XiaoJie <guoxiaojie@onchain.com>
2019-03-20 15:29:25 +0800

    Add test case.
    Add test case.
    
    Signed-off-by: Guo XiaoJie <guoxiaojie@onchain.com>

---
+++ transfer/dijkstra_test.go
@@ -0,0 +1,74 @@
+package transfer
+
+import (
+	"testing"
+
+	chainComm "github.com/oniio/oniChain/common"
+	"fmt"
+	"github.com/oniio/oniChannel/common"
+	"github.com/oniio/oniChannel/common/constants"
+)
+
+func indexOf(element string, data []string) int {
+	for k, v := range data {
+		if element == v {
+			return k
+		}
+	}
+	return -1 //not found.
+}
+
+func TestSPT(t *testing.T) {
+	nodes := make(map[common.Address]int64)
+	names := []string{
+		"AGeTrARjozPVLhuzMxZq36THMtvsrZNAHq",
+		"AYMnqA65pJFKAbbpD8hi5gdNDBmeFBy5hS",
+		"AJtzEUDLzsRKbHC1Tfc1oNh8a1edpnVAUf",
+		"AWpW2ukMkgkgRKtwWxC3viXEX8ijLio2Ng",
+		"AMkN2sRQyT3qHZQqwEycHCX2ezdZNpXNdJ",
+	}
+	for _, n := range names {
+		addr, _ := chainComm.AddressFromBase58(n)
+		nodes[common.Address(addr)] = 0
+	}
+	fmt.Println(nodes)
+
+	edges := make(map[common.EdgeId]int64)
+
+	edgeNames := make([][]string, 0)
+	edgeNames = append(edgeNames, []string{"AGeTrARjozPVLhuzMxZq36THMtvsrZNAHq", "AYMnqA65pJFKAbbpD8hi5gdNDBmeFBy5hS"})
+	edgeNames = append(edgeNames, []string{"AYMnqA65pJFKAbbpD8hi5gdNDBmeFBy5hS", "AGeTrARjozPVLhuzMxZq36THMtvsrZNAHq"})
+	edgeNames = append(edgeNames, []string{"AJtzEUDLzsRKbHC1Tfc1oNh8a1edpnVAUf", "AWpW2ukMkgkgRKtwWxC3viXEX8ijLio2Ng"})
+	edgeNames = append(edgeNames, []string{"AWpW2ukMkgkgRKtwWxC3viXEX8ijLio2Ng", "AJtzEUDLzsRKbHC1Tfc1oNh8a1edpnVAUf"})
+	edgeNames = append(edgeNames, []string{"AMkN2sRQyT3qHZQqwEycHCX2ezdZNpXNdJ", "AJtzEUDLzsRKbHC1Tfc1oNh8a1edpnVAUf"})
+	edgeNames = append(edgeNames, []string{"AJtzEUDLzsRKbHC1Tfc1oNh8a1edpnVAUf", "AMkN2sRQyT3qHZQqwEycHCX2ezdZNpXNdJ"})
+
+	for _, en := range edgeNames {
+		NodeA := en[0]
+		NodeB := en[1]
+		addrA, _ := chainComm.AddressFromBase58(NodeA)
+		addrB, _ := chainComm.AddressFromBase58(NodeB)
+
+		var nodeANodeB common.EdgeId
+		copy(nodeANodeB[:constants.ADDR_LEN], addrA[:])
+		copy(nodeANodeB[constants.ADDR_LEN:], addrB[:])
+		edges[nodeANodeB] = 1
+
+		var nodeBNodeA common.EdgeId
+		copy(nodeBNodeA[:constants.ADDR_LEN], addrB[:])
+		copy(nodeBNodeA[constants.ADDR_LEN:], addrA[:])
+		edges[nodeBNodeA] = 1
+	}
+	fmt.Println(edges)
+
+	toAddress, err := chainComm.AddressFromBase58("AMkN2sRQyT3qHZQqwEycHCX2ezdZNpXNdJ")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	top := NewTopology(nodes, edges)
+	fmt.Println("TOP: ", top)
+	spt := top.GetShortPath(common.Address(toAddress))
+	fmt.Println("SPT:", spt)
+
+}
---
+++ transfer/state_test.go
@@ -0,0 +1,42 @@
+package transfer
+
+import (
+	"testing"
+	"encoding/json"
+	"fmt"
+	"github.com/oniio/oniChannel/common"
+)
+
+
+type TokenNetwork struct {
+	Nodes map[common.Address]int64
+	Edges map[common.EdgeId]int64
+}
+
+func TestTokenNetworkGraphMarshal(t *testing.T) {
+	var a TokenNetwork
+	a.Nodes = make(map[common.Address]int64)
+	a.Edges = make(map[common.EdgeId]int64)
+
+	var addr common.Address
+	a.Nodes[addr] = 1
+
+	var edge common.EdgeId
+	a.Edges[edge] = 2
+
+	data, err := json.Marshal(a)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	fmt.Println(data)
+
+	var b TokenNetwork
+	b.Nodes = make(map[common.Address]int64)
+	b.Edges = make(map[common.EdgeId]int64)
+
+	err = json.Unmarshal(data, &b)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	fmt.Println(b)
+}
