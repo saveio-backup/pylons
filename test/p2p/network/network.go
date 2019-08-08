2732cdf350233e735855ee32f723c72d608ed6c4
Guo XiaoJie <guoxiaojie@onchain.com>
2019-08-08 17:33:32 +0800

    Update test for testnet.
    Update test for testnet.
    
    Signed-off-by: Guo XiaoJie <guoxiaojie@onchain.com>

--- test/p2p/network/network.go
+++ test/p2p/network/network.go
@@ -20,11 +20,11 @@ import (
 	"github.com/saveio/dsp-go-sdk/network/common"
 	"github.com/saveio/dsp-go-sdk/network/message/pb"
 	dspmsg "github.com/saveio/dsp-go-sdk/network/message/pb"
-	"github.com/saveio/edge/common/config"
 	act "github.com/saveio/pylons/actor/server"
 	"github.com/saveio/pylons/network/transport/messages"
 	pm "github.com/saveio/scan/messages/protoMessages"
 	"github.com/saveio/themis/common/log"
+	"github.com/saveio/pylons/test/testnet/test_config"
 )
 
 var once sync.Once
@@ -209,13 +209,13 @@ func (this *Network) Start(address string) error {
 		opcode.RegisterMessageType(opcode.Opcode(common.MSG_OP_CODE), &pb.Message{})
 	})
 
-	if len(config.Parameters.BaseConfig.NATProxyServerAddrs) > 0 {
+	if len(test_config.Parameters.BaseConfig.NATProxyServerAddrs) > 0 {
 		this.P2p.EnableProxyMode(true)
-		this.P2p.SetProxyServer(config.Parameters.BaseConfig.NATProxyServerAddrs)
+		this.P2p.SetProxyServer(test_config.Parameters.BaseConfig.NATProxyServerAddrs)
 	}
 
-	log.Infof("network Id : %d", config.Parameters.BaseConfig.NetworkId)
-	this.P2p.SetNetworkID(config.Parameters.BaseConfig.NetworkId)
+	log.Infof("network Id : %d", test_config.Parameters.BaseConfig.NetworkId)
+	this.P2p.SetNetworkID(test_config.Parameters.BaseConfig.NetworkId)
 	go this.P2p.Listen()
 	// go this.PeerStateChange(this.syncPeerState)
 	this.P2p.BlockUntilListening()
@@ -230,7 +230,7 @@ func (this *Network) Start(address string) error {
 }
 
 func (this *Network) AddProxyComponents(builder *network.Builder) {
-	servers := strings.Split(config.Parameters.BaseConfig.NATProxyServerAddrs, ",")
+	servers := strings.Split(test_config.Parameters.BaseConfig.NATProxyServerAddrs, ",")
 	hasAdd := make(map[string]struct{})
 	for _, proxyAddr := range servers {
 		if len(proxyAddr) == 0 {
@@ -257,8 +257,8 @@ func (this *Network) AddProxyComponents(builder *network.Builder) {
 
 func (this *Network) StartProxy(builder *network.Builder) error {
 	var err error
-	log.Debugf("NATProxyServerAddrs :%v", config.Parameters.BaseConfig.NATProxyServerAddrs)
-	servers := strings.Split(config.Parameters.BaseConfig.NATProxyServerAddrs, ",")
+	log.Debugf("NATProxyServerAddrs :%v", test_config.Parameters.BaseConfig.NATProxyServerAddrs)
+	servers := strings.Split(test_config.Parameters.BaseConfig.NATProxyServerAddrs, ",")
 	for _, proxyAddr := range servers {
 		if len(proxyAddr) == 0 {
 			continue
@@ -266,7 +266,7 @@ func (this *Network) StartProxy(builder *network.Builder) error {
 		this.P2p.EnableProxyMode(true)
 		this.P2p.SetProxyServer(proxyAddr)
 		protocol := getProtocolFromAddr(proxyAddr)
-		log.Debugf("start proxy will blocking...%s %s, networkId: %d", protocol, proxyAddr, config.Parameters.BaseConfig.NetworkId)
+		log.Debugf("start proxy will blocking...%s %s, networkId: %d", protocol, proxyAddr, test_config.Parameters.BaseConfig.NetworkId)
 		done := make(chan struct{}, 1)
 		go func() {
 			switch protocol {
--- test/testnet/test_config/config.go
+++ test/testnet/test_config/config.go
@@ -3,6 +3,7 @@ package test_config
 import (
 	ch "github.com/saveio/pylons"
 	"github.com/saveio/pylons/common"
+	"github.com/saveio/edge/common/config"
 )
 
 const PROTOCOL = "tcp"
@@ -31,6 +32,8 @@ var DNS1 = &ch.ChannelConfig{
 var Dns1Addr common.Address
 var Initiator1Addr common.Address
 var Initiator2Addr common.Address
+var Parameters *config.DspConfig
+
 
 func init() {
 	Dns1AddrTmp, _ := common.FromBase58("AXUhmdzcAJwaFW91q6UYuPGGJY3fimoTAj")
@@ -40,4 +43,46 @@ func init() {
 	Dns1Addr = common.Address(Dns1AddrTmp)
 	Initiator1Addr = common.Address(Initiator1AddrTmp)
 	Initiator2Addr = common.Address(Initiator2AddrTmp)
+	Parameters = TestConfig()
+}
+
+
+func TestConfig() *config.DspConfig {
+	return &config.DspConfig{
+		BaseConfig: config.BaseConfig{
+			NetworkId:            1564141146,
+			BaseDir:              ".",
+			LogPath:              "./Log",
+			PortBase:             10000,
+			LogLevel:             0,
+			LocalRpcPortOffset:   205,
+			EnableLocalRpc:       false,
+			JsonRpcPortOffset:    204,
+			EnableJsonRpc:        true,
+			HttpRestPortOffset:   203,
+			RestEnable:           true,
+			ChannelPortOffset:    202,
+			ChannelProtocol:      "udp",
+			ChannelClientType:    "rpc",
+			ChannelRevealTimeout: "250",
+			DBPath:               "./DB",
+			ChainRestAddrs:       []string{"http://127.0.0.1:20334"},
+			ChainRpcAddrs:        []string{"http://221.179.156.57:10336"},
+			NATProxyServerAddrs:  "",
+			DspProtocol:          "udp",
+			DspPortOffset:        201,
+			AutoSetupDNSEnable:   true,
+			DnsNodeMaxNum:        100,
+			SeedInterval:         3600,
+			DnsChannelDeposit:    1000000000,
+			WalletPwd:            "pwd",
+			WalletDir:            "./wallet.dat",
+		},
+		FsConfig: config.FsConfig{
+			FsRepoRoot:   "./FS",
+			FsFileRoot:   "./Downloads",
+			FsType:       0,
+			EnableBackup: true,
+		},
+	}
 }
