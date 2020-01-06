69fd05392ff17cff05a74d21e3e3a1548f1e0ead
Xiang Fu <fuxiang@onchain.com>
2020-01-06 13:43:17 +0800

    add route algo and test cases

--- transfer/dijkstra.go
+++ transfer/dijkstra.go
@@ -1,16 +1,16 @@
 package transfer
 
 import (
-	"container/list"
+	"sync"
 
 	"github.com/saveio/pylons/common"
-	"sync"
 )
 
 // Topology represents a network topology
 type Topology struct {
-	nodes map[common.Address]int64
-	edges map[common.Address]map[common.Address]int64
+	nodes map[common.Address]int
+	edges map[common.Address]map[common.Address]int
+	sp    ShortPathTree
 }
 
 // Edge represents a directed edge in a graph
@@ -20,17 +20,21 @@ type Edge struct {
 	Distance int64
 }
 
+const (
+	INT_MAX = 1<<32 - 1
+)
+
 type ShortPathTree [][]common.Address
 
 // NewTopology creates a new topology
 func NewTopology(nodes *sync.Map, edges *sync.Map) *Topology {
 	t := &Topology{
-		nodes: make(map[common.Address]int64),
-		edges: make(map[common.Address]map[common.Address]int64),
+		nodes: make(map[common.Address]int),
+		edges: make(map[common.Address]map[common.Address]int),
 	}
 
 	nodes.Range(func(key, value interface{}) bool {
-		t.nodes[key.(common.Address)] = -1
+		t.nodes[key.(common.Address)] = value.(int)
 		return true
 	})
 
@@ -38,74 +42,62 @@ func NewTopology(nodes *sync.Map, edges *sync.Map) *Topology {
 		addr1 := key.(common.EdgeId).GetAddr1()
 		addr2 := key.(common.EdgeId).GetAddr2()
 		if _, ok := t.edges[addr1]; !ok {
-			t.edges[addr1] = make(map[common.Address]int64)
+			t.edges[addr1] = make(map[common.Address]int)
 		}
 
-		t.edges[addr1][addr2] = value.(int64)
+		t.edges[addr1][addr2] = value.(int)
 		return true
 	})
 	return t
 }
 
 func (self *Topology) GetShortPath(node common.Address) ShortPathTree {
-	var sp ShortPathTree
-	lst := list.New()
-	for n := range self.nodes {
-		if n != node {
-			lst.PushBack(n)
+	if 0 == len(self.nodes) || 0 == len(self.edges) {
+		return [][]common.Address{}
+	}
+	var path []common.Address
+	self.searchPath(node, path)
+	return self.sp
+}
+
+func (self *Topology) searchPath(node common.Address, path []common.Address) {
+	path = append(path, node)
+	for _, v := range self.sp {
+		if sliceEqual(v, path) {
+			return
+		}
+	}
+	self.sp = append(self.sp, path)
+	for n := range self.edges[node] {
+		//fmt.Printf("int node %s loop\n", common.ToBase58(node))
+		walked := false
+		for _, v := range path {
+			if n == v {
+				//fmt.Printf("node %s exist break\n", common.ToBase58(n))
+				walked = true
+			}
+		}
+		if walked {
+			continue
+		}
+		self.searchPath(n, path)
+	}
+	return
+}
+func sliceEqual(a, b []common.Address) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	if (a == nil) != (b == nil) {
+		return false
+	}
+
+	for i, v := range a {
+		if v != b[i] {
+			return false
 		}
 	}
 
-	var lastLstLen = lst.Len()
-	for e := lst.Front(); lst.Len() != 0; e = e.Next() {
-		if e == nil {
-			e = lst.Front()
-		}
-
-		n := e.Value.(common.Address)
-		//fmt.Println("Node | n : ", node, "| ", n)
-		if _, exist1 := self.edges[node][n]; exist1 {
-			var path []common.Address
-			path = append(path, n)
-			sp = append(sp, path)
-			//fmt.Println("Remove: ", e.Value.(common.Address))
-			//fmt.Println("Path: ", path)
-			lst.Remove(e)
-		} else {
-			existPath := false
-			pathCount := len(sp)
-			for i := 0; i < pathCount; i++ {
-				tmpPathLen := len(sp[i])
-				pathLastNode := sp[i][tmpPathLen-1]
-				//fmt.Println("PathLastNode |  n : ", pathLastNode, "| ", n)
-				if _, exist2 := self.edges[pathLastNode]; exist2 {
-					//fmt.Println("exist2")
-					mp := self.edges[pathLastNode]
-					//fmt.Println("mp: ", mp)
-					if _, exist3 := mp[n]; exist3 {
-						existPath = true
-						//fmt.Println("exist3")
-						path := make([]common.Address, len(sp[i]))
-						copy(path, sp[i])
-						path = append(path, n)
-						sp = append(sp, path)
-						//fmt.Println("Path: ", path)
-					}
-				}
-			}
-			if existPath {
-				//fmt.Println("Remove: ", e.Value.(common.Address))
-				lst.Remove(e)
-			}
-		}
-
-		if e == lst.Back() && lst.Len() == lastLstLen {
-			//fmt.Println(lastLstLen)
-			break
-		}
-
-		lastLstLen = lst.Len()
-	}
-	//fmt.Println(sp)
-	return sp
+	return true
 }
